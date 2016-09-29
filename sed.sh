7e41c4f2ab532eca0ef3f28a847ddfe56f374a3c
killinux <killinux@163.com>
2016-09-29 22:40:05 +0800

    sed.sh

--- sed.sh
+++ sed.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #sed -i "s/192.168.8.61/192.168.8.61/g" `grep "192.168.8.61" -rl  .` 
 #sed -i "s/192.168.0.110/192.168.8.61/g" `grep  "192.168.0.110" -rl  .|grep -v "sed.sh" `
-sed -i "s/192.168.8.61/killinux.com/g" `grep  "192.168.8.61" -rl  .|grep -v "sed.sh" 
+sed -i "s/192.168.8.61/killinux.com/g" `grep  "192.168.8.61" -rl  .|grep -v "sed.sh" `
